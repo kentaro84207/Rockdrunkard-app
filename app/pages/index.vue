c73e86658db740af4050ec5e88a548727a8bb49e
kentaro84207 <kentaro84207@gmail.com>
2019-07-24 23:09:04 +0900

    add color

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,33 +6,31 @@
         <div @click="signOut()">Sign Out</div>
         <v-flex xs12 sm8 md6>
           <ul>
-            <li v-for="problem in problems" :key="problem.pid">
-              <v-card color="v0" class="white--text">
+            <li v-for="problem in problems" :key="problem.pid" class="pb-3">
+              <v-card :color="difficultyColor[problem.difficulty]" class="black--text">
                 <v-layout>
                   <v-flex xs7>
-                    <v-card-title primary-title>
+                    <v-card-title primary-title class="pl-3 py-2">
                       <div>
-                        <div class="headline">No.1</div>
-                        <div>セッター</div>
-                        <div>{{ problem.setted_by }}</div>
+                        <div class="headline">No.{{ String(problem.pid).slice(-2) }}</div>
+                        <div>セッター：{{ problem.setted_by }}</div>
                       </div>
                     </v-card-title>
                   </v-flex>
                   <v-flex xs5>
-                    <v-card-title primary-title>
+                    <v-card-title primary-title class="pl-3 py-2">
                       <div>
-                        <div class="headline">{{ problem.difficulty }}</div>
-                        <div>完登数</div>
-                        <div>10</div>
+                        <div class="headline">{{ difficulty[problem.difficulty] }}</div>
+                        <div>完登人数：10</div>
                       </div>
                     </v-card-title>
                   </v-flex>
                 </v-layout>
                 <v-divider light></v-divider>
-                <v-card-actions class="pa-3">
-                  <div class="test">完登日</div>
-                  <div>2019/07/03</div>
-                  <v-spacer></v-spacer>
+                <v-card-actions class="pl-3 py-0">
+                  <!-- <div class="test">完登日</div> -->
+                  <!-- <div>2019/07/03</div> -->
+                  <!-- <v-spacer></v-spacer> -->
                   <v-checkbox label="完登"></v-checkbox>
                 </v-card-actions>
               </v-card>
@@ -51,6 +49,28 @@ import { Problem } from '~/types/problem'
 
 @Component({})
 export default class Top extends Vue {
+  difficulty: { [key: number]: string; } = {
+    0: '〜7級',
+    1: '6級',
+    2: '5級',
+    3: '4級',
+    4: '3級',
+    5: '2級',
+    6: '1級',
+    7: '初段〜',
+  }
+
+  difficultyColor: { [key: number]: string; } = {
+    0: 'r0',
+    1: 'r1',
+    2: 'r2',
+    3: 'r3',
+    4: 'r4',
+    5: 'r5',
+    6: 'r6',
+    7: 'r7',
+  }
+
   async created() {
     await this.$store.dispatch('problem/getProblems')
   }
@@ -87,5 +107,4 @@ export default class Top extends Vue {
 </script>
 
 <style lang='scss' scoped >
-
 </style>
