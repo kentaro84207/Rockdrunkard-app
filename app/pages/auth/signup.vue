9319a09e90fe283fac1162b0a256c3ad439bd343
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2019-07-29 22:48:07 +0900

    :sparkles: mail login (#3)
    * add: mail login
    
    * add: signup link

--- app/pages/auth/signup.vue
+++ app/pages/auth/signup.vue
@@ -5,7 +5,7 @@
         <v-flex xs12 md4>
           <input type="text" placeholder="E-mail" v-model="email" />
           <input type="password" placeholder="Password" v-model="password" />
-          <div @click="signupWithEmailAndPassword">Register!</div>
+          <div @click="signupWithEmailAndPassword()">Register!</div>
         </v-flex>
       </v-layout>
     </v-container>
@@ -14,23 +14,22 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import firebase from '~/plugins/firebase'
+import { firebaseAuth } from '~/plugins/firebase'
 
 @Component({
   layout: 'login'
 })
-export default class EmailSignup extends Vue {
+export default class Signup extends Vue {
   email: string = ''
   password: string = ''
 
-  signupWithEmailAndPassword() {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.email, this.password)
-      .catch(error => {
-        alert(error.message)
-        console.log('error')
-      })
+  async signupWithEmailAndPassword() {
+    try{
+      await firebaseAuth.createUserWithEmailAndPassword(this.email, this.password)
+      this.$router.replace('/')
+    } catch(error){
+      console.log('Signup error', error)
+    }
   }
 }
 </script>
