da8c8879f5932bf9006032540dbcfa55f5d1b816
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2019-09-28 10:13:36 +0900

    :art: Login & Signup (#7)
    * Add: login design
    
    * Add: signup design
    
    * Change: event
    
    * Fix: use data

--- app/store/user.ts
+++ app/store/user.ts
@@ -1,47 +1,48 @@
-import { User } from '~/types/user';
-import { MutationTree, GetterTree, ActionTree } from 'vuex';
-import firestore from '~/plugins/firestore';
-import { firebaseAuth, GoogleAuthProvider } from '~/plugins/firebase';
+import { User } from '~/types/user'
+import { MutationTree, GetterTree, ActionTree } from 'vuex'
+import firestore from '~/plugins/firestore'
+import { firebaseAuth, GoogleAuthProvider } from '~/plugins/firebase'
 
 interface State {
   // loading: boolean;
-  user: User;
+  user: User
 }
 
 export const state = (): State => ({
   // loading: false,
   user: null
-});
+})
 
 export const mutations: MutationTree<State> = {
   // setLoading(state: State, loading: boolean): void {
   //   state.loading = loading;
   // },
   setUser(state: State, payload: User): void {
-    state.user = payload;
-  }
-};
+    state.user = payload
+  },
+}
 
 export const getters: GetterTree<State, State> = {
   isAuthenticated(state) {
     return !!state.user
-  },
+  }
   // user(state) {
   //   return state.user
   // }
-};
+}
 
 export const actions: ActionTree<State, State> = {
-  authStateChange({ commit }, payload) {
+  async authStateChange({ commit }, payload) {
     // commit('setLoading', true);
+    const userRef = firestore.collection('users').doc(payload.uid)
+    const userSnap = await userRef.get()
+    const userDate = userSnap.data()
 
     const user: User = {
-      ...payload.providerData[0],
-      emailVerified: payload.emailVerified,
-      isAnonymous: payload.isAnonymous,
-      uid: payload.uid
-    };
-    commit('setUser', user);
+      nickname: userDate.nickname,
+      uid: userDate.uid
+    }
+    commit('setUser', user)
     // commit('setLoading', false);
   },
 
@@ -58,7 +59,7 @@ export const actions: ActionTree<State, State> = {
   // },
 
   async signOut({ commit }) {
-    await firebaseAuth.signOut();
-    commit('setUser', null);
+    await firebaseAuth.signOut()
+    commit('setUser', null)
   }
-};
+}
--- app/types/user.ts
+++ app/types/user.ts
@@ -1,11 +1,4 @@
 export interface User {
-  uid: string;
-  displayName: string | null;
-  email: string | null;
-  phoneNumber: string | null;
-  photoURL: string | null;
-  providerId: string;
-  emailVerified: boolean;
-  isAnonymous: boolean;
-  isAdmin: boolean;
+  nickname: string | null
+  uid: string | null
 }
