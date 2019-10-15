fdc1e0d90eb6097e4fd7649c0c6291c00b7a0a3e
kentaro84207 <kentaro84207@gmail.com>
2019-10-15 21:17:04 +0900

    Fix: bottom nav

--- app/components/TheNavigation.vue
+++ app/components/TheNavigation.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-bottom-navigation :value="activeBtn" grow fixed color="primary">
-    <v-btn color="white" to="/" nuxt>
+  <v-bottom-navigation :value="bottomNav" grow fixed color="primary">
+    <v-btn color="white" value="list" to="/" nuxt>
       <span>課題</span>
       <v-icon>view_headline</v-icon>
     </v-btn>
-    <v-btn color="white" to="/ranking" nuxt>
+    <v-btn color="white" value="ranking" to="/ranking" nuxt>
       <span>順位</span>
       <v-icon>trending_up</v-icon>
     </v-btn>
-    <v-btn color="white" to="/setting" nuxt>
+    <v-btn color="white" value="setting" to="/setting" nuxt>
       <span>設定</span>
       <v-icon>more_horiz</v-icon>
     </v-btn>
@@ -20,6 +20,6 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component({})
 export default class navigation extends Vue {
-  activeBtn = 1
+  bottomNav: string = 'list'
 }
 </script>
