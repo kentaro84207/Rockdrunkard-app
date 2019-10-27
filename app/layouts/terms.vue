dc5a157358c51d58c2bb8c9fcef522f2d1686117
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2019-10-27 22:12:36 +0900

    :sparkles: Terms and Privacy (#13)
    * Add: privacy and terms
    
    * Add: terms and privacy

---
+++ app/layouts/terms.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-content class="">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class TermsLayout extends Vue {}
+</script>
---
+++ app/pages/others/privacy.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid grid-list-lg mb-12 pb-12>
+    <Privacy></Privacy>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Privacy from '~/components/Privacy.vue'
+
+@Component({
+  components: {
+    Privacy
+  }
+})
+export default class OthersPrivacy extends Vue {}
+</script>
+
+<style lang='scss' scoped >
+section {
+  margin-bottom: 30px;
+}
+
+ul,
+ol {
+  margin-top: 10px;
+}
+
+ul,
+ol,
+li {
+  margin-bottom: 10px;
+}
+</style>
